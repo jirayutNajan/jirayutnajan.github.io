c5a52d7b6959fb75492c084de096705a971d7b75
Jirayut Najan <tonklaeiei123@gmail.com>
2025-01-27 00:45:54 +0700

    fix phone view

---
+++ js/app.js
@@ -0,0 +1,43 @@
+document.addEventListener('DOMContentLoaded', scrollgrab('.ac1'));
+document.addEventListener('DOMContentLoaded', scrollgrab('.ac2'));
+
+function scrollgrab(classname) {
+    const ele = document.querySelector(classname);
+    ele.style.cursor = 'grab';
+
+    let pos = { top: 0, left: 0, x: 0, y: 0 };
+
+    const mouseDownHandler = function (e) {
+        ele.style.cursor = 'grabbing';
+        ele.style.userSelect = 'none';
+
+        pos = {
+            left: ele.scrollLeft,
+            // Get the current mouse position
+            x: e.clientX,
+        };
+
+        document.addEventListener('mousemove', mouseMoveHandler);
+        document.addEventListener('mouseup', mouseUpHandler);
+    };
+
+    const mouseMoveHandler = function (e) {
+        // How far the mouse has been moved
+        const dx = e.clientX - pos.x;
+
+        // Scroll the element
+        ele.scrollLeft = pos.left - dx;
+    };
+
+    const mouseUpHandler = function () {
+        ele.style.cursor = 'grab';
+        ele.style.removeProperty('user-select');
+
+        document.removeEventListener('mousemove', mouseMoveHandler);
+        document.removeEventListener('mouseup', mouseUpHandler);
+    };
+
+    // Attach the handler
+    ele.addEventListener('mousedown', mouseDownHandler);
+}
+
